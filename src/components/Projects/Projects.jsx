ec2d76a089d81a79b14389ac4913199c9e7119f6
Jolonov07 <zolotoy.baiaman@gmail.com>
2022-10-17 15:43:56 +0600

    update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import ProjectCard from './ProjectCards'
 import Particle from '../Particle'
-import leaf from '../../Assets/Projects/leaf.png'
-import emotion from '../../Assets/Projects/emotion.png'
-import editor from '../../Assets/Projects/codeEditor.png'
-import chatify from '../../Assets/Projects/chatify.png'
-import suicide from '../../Assets/Projects/suicide.png'
-import bitsOfCode from '../../Assets/Projects/blog.png'
+import sticky from '../../Assets/Projects/sticky.png'
+import calculator from '../../Assets/Projects/calculator.png'
+import todo from '../../Assets/Projects/todo.png'
+import movie from '../../Assets/Projects/movie.png'
+import sneakers from '../../Assets/Projects/sneakers.png'
+import note from '../../Assets/Projects/thenote.png'
 
 const Projects = () => {
 	return (
@@ -23,68 +23,67 @@ const Projects = () => {
 				<Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
 					<Col md={4} className='project-card'>
 						<ProjectCard
-							imgPath={chatify}
+							imgPath={movie}
 							isBlog={false}
-							title='Chatify'
-							description='Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages.'
-							ghLink='https://github.com/soumyajit4419/Chatify'
-							demoLink='https://chatify-49.web.app/'
+							title='MOVIE APP'
+							description='Movie App'
+							ghLink='https://github.com/Jolonov07/react-movie-app'
+							demoLink='https://react-movie-app-jolonov07.vercel.app'
 						/>
 					</Col>
 
 					<Col md={4} className='project-card'>
 						<ProjectCard
-							imgPath={bitsOfCode}
+							imgPath={sneakers}
 							isBlog={false}
-							title='Bits-0f-C0de'
-							description='My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown.'
-							ghLink='https://github.com/soumyajit4419/Bits-0f-C0de'
-							demoLink='https://blogs.soumya-jit.tech/'
+							title='SNEAKER SHOP'
+							description='Sneaker Shop'
+							ghLink='https://github.com/Jolonov07/React-Sneaker-Shop'
+							demoLink='https://react-sneaker-shop-wine.vercel.app/'
 						/>
 					</Col>
 
 					<Col md={4} className='project-card'>
 						<ProjectCard
-							imgPath={editor}
+							imgPath={note}
 							isBlog={false}
-							title='Editor.io'
-							description='Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage'
-							ghLink='https://github.com/soumyajit4419/Editor.io'
-							demoLink='https://editor.soumya-jit.tech/'
+							title='THE NOTE'
+							description='The Note'
+							ghLink='https://github.com/Jolonov07/react-the-note'
+							demoLink='https://react-the-note.vercel.app/'
 						/>
 					</Col>
 
 					<Col md={4} className='project-card'>
 						<ProjectCard
-							imgPath={leaf}
+							imgPath={todo}
 							isBlog={false}
-							title='Plant AI'
-							description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-							ghLink='https://github.com/soumyajit4419/Plant_AI'
-							demoLink='https://plant49-ai.herokuapp.com/'
+							title='TODO LIST'
+							description="TODO LIST"
+							ghLink='https://github.com/Jolonov07/React-TodoList'
+							demoLink='https://react-todo-firebase-fawn.vercel.app/'
 						/>
 					</Col>
 
 					<Col md={4} className='project-card'>
 						<ProjectCard
-							imgPath={suicide}
+							imgPath={sticky}
 							isBlog={false}
-							title='Ai For Social Good'
-							description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-							ghLink='https://github.com/soumyajit4419/AI_For_Social_Good'
-							// demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+							title='STICKY NOTES'
+							description="Sticky Notes"
+							ghLink='https://github.com/Jolonov07/Sticky-Notes-Using-JS'
+							demoLink="https://sticky-notes-using-js.vercel.app/"
 						/>
 					</Col>
 
 					<Col md={4} className='project-card'>
 						<ProjectCard
-							imgPath={emotion}
+							imgPath={calculator}
 							isBlog={false}
-							title='Face Recognition and Emotion Detection'
-							description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-							ghLink='https://github.com/soumyajit4419/Face_And_Emotion_Detection'
-							// demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+							title='CALCULATOR'
+							description="Calculator"
+							ghLink='https://github.com/Jolonov07/Calculator-js'
+							demoLink="https://calculator-js-kappa.vercel.app/"
 						/>
 					</Col>
 				</Row>
